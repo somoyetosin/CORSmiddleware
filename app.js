1e78d7154e2a2e575b69d39e4c9180af8c3a882c
Martins Somoye <martins@somoyetosin.com>
2017-12-23 02:07:29 -0500

    Added Authorization to the Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(jsonParser());
 //middleware for cross origin request
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if(req.method === "OPTIONS"){
         res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, PATCH, GET");
         return res.status(200).json({});
